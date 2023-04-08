94c911c709550ff92acdac7094b3be7fbc4072bc
Anunay Sinha <anusu90@gmail.com>
2023-04-08 20:34:05 +0530

    polymorphism

---
+++ src/main/java/com/anunay/polymorphism/User.java
@@ -0,0 +1,10 @@
+package com.anunay.polymorphism;
+
+public class User {
+    private String email;
+    private String password;
+
+    public void changePassword(String newPass){
+        password=newPass;
+    }
+}
